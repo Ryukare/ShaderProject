b346664ffbf117d6fada9885f449e65796edb40f
Oskar Rojek <s25723@pjwstk.edu.pl>
2024-04-29 14:47:23 +0200

    Imported new assets and created Tilemap

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,30 +1,32 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Sprites")]
+    [SerializeField] private SpriteRenderer bodySpriteRenderer;
+    [SerializeField] private SpriteRenderer rightLegSpriteRenderer;
+    [SerializeField] private SpriteRenderer leftLegSpriteRenderer;
     
+    [Header("Parameters")]
     [SerializeField] private float speed;
     
-    private Rigidbody2D _rigidbody2D;
-    //private SpriteRenderer _spriteRenderer;
+    private Rigidbody _rigidbody;
+    
     private Animator _animator;
     private float _xInput;
-    private float _yInput;
+    private float _zInput;
+    
     private void Awake()
     {
-        _rigidbody2D = GetComponent<Rigidbody2D>();
-        //_spriteRenderer = GetComponent<SpriteRenderer>();
+        _rigidbody = GetComponent<Rigidbody>();
         _animator = GetComponent<Animator>();
     }
     
     private void Update()
     {
         _xInput = Input.GetAxis("Horizontal");
-        _yInput = Input.GetAxis("Vertical");
-        if (_xInput != 0 || _yInput != 0)
+        _zInput = Input.GetAxis("Vertical");
+        if (_xInput != 0 || _zInput != 0)
         {
             _animator.SetBool("isRunning",true);
         }
@@ -36,17 +38,29 @@ public class PlayerMovement : MonoBehaviour
     
     private void FixedUpdate()
     {
-        _rigidbody2D.velocity = new Vector2(_xInput, _yInput).normalized * speed;
+        _rigidbody.velocity = new Vector3(_xInput, 0, _zInput).normalized * speed;
         //FlipX();
     }
     
     // private void FlipX()
     // {
-    //     _spriteRenderer.flipX = _xInput switch
+    //     bodySpriteRenderer.flipX = _xInput switch
     //     {
     //         > 0 => false,
     //         < 0 => true,
-    //         _ => _spriteRenderer.flipX
+    //         _ => bodySpriteRenderer.flipX
+    //     };
+    //     rightLegSpriteRenderer.flipX = _xInput switch
+    //     {
+    //         > 0 => false,
+    //         < 0 => true,
+    //         _ => rightLegSpriteRenderer.flipX
+    //     };
+    //     leftLegSpriteRenderer.flipX = _xInput switch
+    //     {
+    //         > 0 => false,
+    //         < 0 => true,
+    //         _ => leftLegSpriteRenderer.flipX
     //     };
     // }
 }
